6aefdb102282f01677437a19769fa96fef7b8cb4
user <user@DESKTOP-MOUJJKK>
2019-12-05 01:37:00 +0530

    My 2nd commit

---
+++ Test_Selenium1/src/testBrowser/siteTest.java
@@ -0,0 +1,13 @@
+package testBrowser;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class siteTest
+{
+	@Test
+	public void creste_User()
+	{
+		Reporter.log("Create user", true);
+	}
+}
